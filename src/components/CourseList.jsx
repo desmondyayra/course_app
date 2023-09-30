78ba3ff29c5ef7be50dd64ce720fe20e3875beb8
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2023-09-30 01:07:13 -0500

    styling the app

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CourseLine from './course';
+import '../App.css';
 
 const CourseList = ({ courses }) => (
-  <div>
-    {Object.entries(courses).map(([id, course]) => (
-      <CourseLine key={id} course={course} />
-    ))}
-
+  <div className="container">
+    <div className="card-grid">
+      {Object.entries(courses).map(([id, course]) => (
+        <CourseLine key={id} course={course} />
+      ))}
+    </div>
   </div>
 );
 
--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const CourseLine = ({ course }) => (
-  <p>
-    {course.term} CS {course.number} : {course.title}
-  </p>
+  <div className="col-md-3 mb-3">
+    <div className="card">
+      <div className="card-header">
+        <h5 className="mb-0">{course.term} CS {course.number}</h5>
+      </div>
+      <div className="card-body">
+        <p className="card-title">{course.title}</p>
+      </div>
+      <div className="card-footer">
+        {course.meets}
+      </div>
+    </div>
+  </div>
 );
 
 export default CourseLine;
