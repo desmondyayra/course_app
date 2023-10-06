7fff841f36e94b196876d83c2224a7cce7be5eba
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2023-10-06 16:26:42 -0500

    selection of courses

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CourseLine from './course';
 import '../App.css';
 
 const CourseList = ({ courses, term }) => {
+  // State to keep track of selected items
+  const [selectedItems, setSelectedItems] = useState([]);
 
-  
-  
+  // Function to toggle selection of an item
+  const toggleSelect = (id) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((item) => item !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  };
+
   const filteredCourses = Object.values(courses).filter(
-    (course) => course.term == term
+    (course) => course.term === term
   );
 
-  
-  console.log(term);
-  
+  // console.log(term);
+
   return (
     <div className="container">
       <div className="card-grid">
         {Object.entries(filteredCourses).map(([id, course]) => (
-          <CourseLine key={id} course={course} />
+          <CourseLine
+            key={id}
+            course={course}
+            isSelected={selectedItems.includes(id)} 
+            toggleSelect={() => toggleSelect(id)} 
+          />
         ))}
       </div>
-    </div>);
+    </div>
+  );
 };
 
 export default CourseList;
--- src/components/course.jsx
+++ src/components/course.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-const CourseLine = ({ course }) => (
-  <div className="col-md-3 mb-3">
-    <div className="card">
-      <div className="card-header">
-        <h5 className="mb-0">{course.term} CS {course.number}</h5>
-      </div>
-      <div className="card-body">
-        <p className="card-title">{course.title}</p>
-      </div>
-      <div className="card-footer">
-        {course.meets}
+const CourseLine = ({ course, isSelected, toggleSelect }) => {
+  // Define a CSS class based on the isSelected prop
+  const cardClass = isSelected ? 'card selected' : 'card';
+
+  return (
+    <div className="col-md-3 mb-3">
+      <div className={cardClass} onClick={toggleSelect}>
+        <div className="card-header">
+          <h5 className="mb-0">{course.term} CS {course.number}</h5>
+        </div>
+        <div className="card-body">
+          <p className="card-title">{course.title}</p>
+        </div>
+        <div className="card-footer">
+          {course.meets}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CourseLine;
+
